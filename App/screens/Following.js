0cf042f9e5ad09abc0f268472a212881b7a5ba3e
amiacastro <acastro@mines.edu>
2023-03-11 13:28:06 -0700

    following start

---
+++ App/screens/Following.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+ class Following extends Component {
+
+  render() {
+    return (
+        <SafeAreaView>
+            <Text>Following</Text>
+        </SafeAreaView>
+      
+    )
+  }
+}
+
+export default Following
