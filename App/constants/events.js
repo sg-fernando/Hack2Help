9bd6fa24a4570f5be10f824207fd1081bc2403db
amiacastro <acastro@mines.edu>
2023-03-11 16:19:26 -0700

    event

--- App/constants/events.js
+++ App/constants/events.js
@@ -5,9 +5,18 @@ const Events = [
         id: 1,
         title: "Event 1",
         description: "This is the first event",
-        date: "2020-01-01",
+        date: "2023-01-01",
         time: "12:00",
         location: "Location 1",
+        image: assets.nft01,
+    }
+    {
+        id: 2,
+        title: "Event 2",
+        description: "This is the second event",
+        date: "2023-02-02",
+        time: "12:00",
+        location: "Location 2",
         image: assets.nft01,
     }
 ]
