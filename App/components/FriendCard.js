fa75bb3e8396967541cb6142ee1b9513d41cab86
Albert <aphan@mines.edu>
2023-03-11 14:38:39 -0700

    ugh

---
+++ App/components/FriendCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Image, TextInput, StyleSheet } from "react-native";
+
+import { COLORS, FONTS, SIZES, assets } from "../constants";
+
+const FriendCard = () => {
+    return (
+        <View>
+            <Image></Image>
+        </View>
+    );
+}
