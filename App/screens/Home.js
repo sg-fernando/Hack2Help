0941f83ab29c981bb44b2278d8ec245bbacc5d06
Albert <aphan@mines.edu>
2023-03-11 14:06:11 -0700

    fixed finally?

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,4 +1,4 @@
-import { Text, View, ImageBackground } from 'react-native'
+import { Text, View, ImageBackground, StyleSheet } from 'react-native'
 import React, { Component } from 'react'
 import { assets } from '../constants';
 
@@ -13,5 +13,14 @@ export class Home extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  image: {
+      flex: 1,
+      justifyContent: 'center',
+      width: '100%',
+      height: '100%',
+  }
+});
 
 export default Home
